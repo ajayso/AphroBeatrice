4f17ab69207b33484c9e9d44e40ee3e446d023a1
Ajay Solanki <ajay.solanki@devfactory.com>
2017-10-02 17:29:31 +0530

    Updated Azure Blob Storage code for python

--- python code/GetStocksAzure.py
+++ python code/GetStocksAzure.py
@@ -2,12 +2,11 @@ import os
 import json
 import sys
 import nsetools
-import pandas
 import datetime
-
 import pandas as pd
 input = pd.read_csv("auto.csv");
-# input.Symbol[i] input.Series.size
+# input.Symbol[i] input.Series.size is the size
+all_stock_codes = input.Symbol
 from nsetools import Nse
 nse = Nse()
 outputlist = []
@@ -20,7 +19,7 @@ for script_code in all_stock_codes:
     # Write to a list 
         except:
                 print("Unexpected error:", sys.exc_info()[0]);
-#Write Outputlist back to Azure Blob Storage
+
 jsonObject = json.dumps(outputlist)
 from azure.storage.blob import AppendBlobService
 append_blob_service = AppendBlobService(account_name='aphrostock', account_key='cwAlI7P6WfUqijt0jMP+0CCqM54hmoHRofxqdj9PqPamtIfdm9vRUpC+jrpRv/Idma61sSg7NDIvFwkyhMm7KQ==')
@@ -28,5 +27,3 @@ containerName = str(datetime.datetime.now())
 append_blob_service.create_blob('stockdata', containerName)
 append_blob_service.append_blob_from_text('stockdata', containerName, jsonObject)
 
-
-response.close()
